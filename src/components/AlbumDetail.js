9e398744e78950c4fa902febdd5a7191892c2474
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-18 18:04:45 -0400

    Display album images

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,19 +5,33 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   // Destructuring for refactor
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles
+  const {
+    title,
+    artist,
+    image,
+    thumbnail_image
+  } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    albumCoverImageStyle
+  } = styles
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={albumCoverImageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -28,9 +42,23 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumCoverImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 });
 
