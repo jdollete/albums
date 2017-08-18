9da45820eea3c1569b807a36dbaf0652866180ad
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-18 14:24:22 -0400

    Adjust styling to header

--- index.ios.js
+++ index.ios.js
@@ -1,23 +1,15 @@
 // Import a libraries
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 
+// Import components
 import Header from './src/components/Header'
 
 
 // Create Components
-// const App = () => (
-//     <Header />
-//   );
-
-class App extends React.Component {
-
-  render() {
-    return (
-      <Header />
-    );
-  }
-}
+const App = () => (
+    <Header />
+  );
 
 // Render to device
 AppRegistry.registerComponent('albums', () => App);
