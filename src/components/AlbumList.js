5a5f2272b30d0e2e8fef3c1ec96f90f99b036c20
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-18 18:12:34 -0400

    Enable scrolling

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -29,9 +29,9 @@ class AlbumList extends React.Component {
   render() {
     console.log(this.state);
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
